964aa8b4986ea202b966d422c9b302b0b4c8ae7b
Adones Pitogo <pitogo.adones@gmail.com>
2025-01-23 19:00:03 +0800

    Squashed 'sdk/utils/' content from commit 161740f4
    git-subtree-dir: sdk/utils
    git-subtree-split: 161740f46e92934f316dac0e1417773698de8b4a

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/flarehotspot/sdk-utils
+
+go 1.19
+
+require github.com/goccy/go-json v0.10.3
---
+++ pkg-plugin-info.go
@@ -0,0 +1,23 @@
+package sdkutils
+
+import (
+	"path/filepath"
+)
+
+type PluginInfo struct {
+	Name           string   `json:"name"`
+	Package        string   `json:"package"`
+	Description    string   `json:"description"`
+	Version        string   `json:"version"`
+	SystemPackages []string `json:"system_packages"`
+	SDK            string   `json:"sdk"`
+}
+
+func GetPluginInfoFromPath(src string) (PluginInfo, error) {
+	var info PluginInfo
+	if err := FsReadJson(filepath.Join(src, "plugin.json"), &info); err != nil {
+		return PluginInfo{}, err
+	}
+
+	return info, nil
+}
