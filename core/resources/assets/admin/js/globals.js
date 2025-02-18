c8791e484fc9104ebb30067fa7a88d18a2e1fce3
Adones Pitogo <pitogo.adones@gmail.com>
2025-02-18 17:14:42 +0800

    core: use relative paths for js lib files

--- core/resources/assets/admin/js/globals.js
+++ core/resources/assets/admin/js/globals.js
@@ -5,11 +5,11 @@ window.$flare = window.$flare || {};
 // Alpine is not a commonjs module
 require('../../lib/vendor/alpinejs-v3.14.3.js');
 
-var htmx = require('@flarehotspot/lib/vendor/htmx-v1.9.12.js')
+var htmx = require('../../lib/vendor/htmx-v1.9.12.js')
 window.htmx = htmx;
 
-require('@flarehotspot/lib/events.js')
-require('@flarehotspot/lib/vendor/htmx-ext-sse-v1.19.12.js')
+require('../../lib/events.js')
+require('../../lib/vendor/htmx-ext-sse-v1.19.12.js')
 require('./notify.js')
 require('./flash.js')
 
--- core/resources/assets/portal/js/globals.js
+++ core/resources/assets/portal/js/globals.js
@@ -2,15 +2,15 @@
 
 window.$flare = window.$flare || {};
 
-var jQuery = require('@flarehotspot/lib/vendor/jquery-v1.12.4.js')
+var jQuery = require('../../lib/vendor/jquery-v1.12.4.js')
 window.$ = jQuery;
 window.jQuery = jQuery;
 
-var htmx = require('@flarehotspot/lib/vendor/htmx-v1.9.12.js');
+var htmx = require('../../lib/vendor/htmx-v1.9.12.js');
 window.htmx = htmx;
 
-require('@flarehotspot/lib/events.js');
-require('@flarehotspot/lib/vendor/htmx-ext-sse-v1.19.12.js');
+require('../../lib/events.js');
+require('../../lib/vendor/htmx-ext-sse-v1.19.12.js');
 require('./notify.js');
 require('./sessions.js');
 require('./flash.js');
