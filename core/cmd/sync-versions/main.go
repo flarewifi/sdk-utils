9d77877652108a1ea94b13ed205f351a60f48328
Adones Pitogo <pitogo.adones@gmail.com>
2025-03-06 00:58:36 +0800

    core: remove all previews release notes

--- core/cmd/sync-versions/main.go
+++ core/cmd/sync-versions/main.go
@@ -14,10 +14,14 @@ func main() {
 	tools.SyncGoVersion()
 	version := plugins.GetCoreInfo().Version
 	releaseNotePath := filepath.Join(sdkutils.PathCoreDir, "build", "release-notes", version+".md")
+
 	if !sdkutils.FsExists(releaseNotePath) {
+		if err := sdkutils.FsEmptyDir(filepath.Dir(releaseNotePath)); err != nil {
+			panic(err)
+		}
+
 		if err := os.WriteFile(releaseNotePath, []byte("## "+version+"\n\n"), 0644); err != nil {
 			panic(err)
 		}
-		return
 	}
 }
