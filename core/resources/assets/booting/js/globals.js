53e8a3f77e71eb6e0cb110e4e837054b9de56ab9
Adones Pitogo <pitogo.adones@gmail.com>
2025-03-24 16:50:24 +0800

    clean up booting js file

--- core/resources/assets/booting/js/globals.js
+++ core/resources/assets/booting/js/globals.js
@@ -1,9 +1,3 @@
 'use strict';
 
-window.$flare = window.$flare || {};
-
-var htmx = require('../../lib/vendor/htmx-v1.9.12.js');
-window.htmx = htmx;
-
-require('../../lib/events.js');
 require('./booting.js');
