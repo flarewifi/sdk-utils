564689bb140ee49aa931fd60ec0991533c69afe0
Adones Pitogo <pitogo.adones@gmail.com>
2025-03-25 18:16:50 +0800

    core: clean up check updates handler

--- core/internal/web/controllers/adminctrl/updates-ctrl.go
+++ core/internal/web/controllers/adminctrl/updates-ctrl.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/Masterminds/semver/v3"
-	sdkutils "github.com/flarehotspot/sdk-utils"
 )
 
 func ShowUpdatesCtrl(g *api.CoreGlobals) http.HandlerFunc {
@@ -53,99 +52,7 @@ func CheckUpdatesCtrl(g *api.CoreGlobals) http.HandlerFunc {
 		}
 
 		time.Sleep(3 * time.Second)
-		page := updatesview.QueryUpdate(api, update, nil)
+		page := updatesview.CheckForUpdate(api, update, nil)
 		page.Render(r.Context(), w)
-	}
-}
-
-func GetCurrentCoreVersionCtrl(g *api.CoreGlobals) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// res := g.CoreAPI.HttpAPI.VueResponse()
-
-		// coreVersion, err := updates.GetCurrentCoreVersion()
-		// if err != nil {
-		// 	log.Println("Error:", err)
-		// 	res.Error(w, err.Error(), http.StatusInternalServerError)
-		// 	return
-		// }
-
-		// res.Json(w, coreVersion, http.StatusOK)
-	}
-}
-
-func DownloadUpdatesCtrl(g *api.CoreGlobals) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// res := g.CoreAPI.HttpAPI.VueResponse()
-
-		// var reqPayload updates.CoreReleaseUpdate
-		// err := json.NewDecoder(r.Body).Decode(&reqPayload)
-		// if err != nil {
-		// 	res.Error(w, err.Error(), http.StatusBadRequest)
-		// 	log.Println("Error reading the request body:", err)
-		// 	return
-		// }
-
-		// stringedVersion := sdksemver.StringifyVersion(reqPayload.Version)
-		// updatesPath := filepath.Join(sdkpaths.CacheDir, "updates", "core", stringedVersion)
-		// coreFilesPath := filepath.Join(updatesPath, "core-files")
-		// archBinFilesPath := filepath.Join(updatesPath, "arch-bin-files")
-
-		// // download core files
-		// err = downloadFile(reqPayload.CoreZipFileUrl, coreFilesPath)
-		// if err != nil {
-		// 	res.Error(w, err.Error(), http.StatusBadRequest)
-		// 	log.Println("Error downloading core files:", err)
-		// 	return
-		// }
-		// // download arch bin files
-		// err = downloadFile(reqPayload.ArchBinFileUrl, archBinFilesPath)
-		// if err != nil {
-		// 	log.Println("Error downloading arch bin files:", err)
-		// 	res.Error(w, err.Error(), http.StatusBadRequest)
-		// 	return
-		// }
-
-		// // TODO: verify downloaded files by checking its checksum
-
-		// // return downloaded local file paths
-		// localUpdateFiles := updates.UpdateFiles{
-		// 	LocalCoreFilesPath:    coreFilesPath,
-		// 	LocalArchBinFilesPath: archBinFilesPath,
-		// }
-
-		// res.Json(w, localUpdateFiles, http.StatusOK)
-	}
-}
-
-func downloadFile(src string, dest string) error {
-	downloader := sdkutils.NewDownloader(src, dest)
-	err := downloader.Download()
-	if err != nil {
-		log.Println("Error:", err)
-		return err
-	}
-
-	return nil
-}
-
-func UpdateCoreCtrl(g *api.CoreGlobals) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// res := g.CoreAPI.HttpAPI.VueResponse()
-
-		// var reqPayload updates.UpdateFiles
-		// err := json.NewDecoder(r.Body).Decode(&reqPayload)
-		// if err != nil {
-		// 	res.Error(w, err.Error(), http.StatusBadRequest)
-		// 	log.Println("Error reading the request body:", err)
-		// 	return
-		// }
-
-		// if err := updates.UpdateCore(reqPayload); err != nil {
-		// 	log.Println("Error:", err)
-		// 	res.Error(w, err.Error(), http.StatusBadRequest)
-		// 	return
-		// }
-
-		// res.Json(w, "Update in progress..", http.StatusOK)
 	}
 }
--- core/resources/views/admin/updates/check-updates_templ.go
+++ core/resources/views/admin/updates/check-updates_templ.go
@@ -57,7 +57,7 @@ func ShowUpdates(api sdkapi.IPluginApi) templ.Component {
 	})
 }
 
-func QueryUpdate(api sdkapi.IPluginApi, update *SoftwareUpdate, err error) templ.Component {
+func CheckForUpdate(api sdkapi.IPluginApi, update *SoftwareUpdate, err error) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
