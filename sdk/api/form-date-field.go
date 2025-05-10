68ef6f4517d6f05858107b00b9748e5adc8b189c
micamics <micastuazon@gmail.com>
2025-05-10 13:40:45 +0800

    Implement date input field.

---
+++ sdk/api/form-date-field.go
@@ -0,0 +1,41 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package sdkapi
+
+const DateFormat = "YYYY-MM-DD"
+
+type FormDateField struct {
+	Name     string
+	Label    string
+	Required bool
+	Minimum  string
+	Maximum  string
+	ValueFn  func() string
+}
+
+func (f FormDateField) GetName() string {
+	return f.Name
+}
+
+func (f FormDateField) GetLabel() string {
+	return f.Label
+}
+
+func (f FormDateField) GetType() string {
+	return FormFieldTypeDate
+}
+
+func (f FormDateField) GetValue() interface{} {
+	if f.ValueFn != nil {
+		return f.ValueFn()
+	}
+	return ""
+}
+
+func (f FormDateField) IsRequired() bool {
+	return f.Required
+}
