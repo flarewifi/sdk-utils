867ad32d6603ddc45b6a3da50c631dfcccd4b231
micamics <micastuazon@gmail.com>
2025-06-08 21:46:57 +0800

    Read mac and ip from cookies for dev build.

--- core/internal/web/helpers/current-client_dev.go
+++ core/internal/web/helpers/current-client_dev.go
@@ -1,4 +1,4 @@
-//go:build !dev
+//go:build dev
 
 package helpers
 
@@ -22,14 +22,34 @@ func CurrentClient(clntMgr *connmgr.ClientRegister, dbpool *pgxpool.Pool, r *htt
 		}
 	}
 
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		return nil, err
+	var ip, mac string
+
+	macval, _ := r.Cookie("mac")
+	ipval, _ := r.Cookie("ip")
+
+	if macval != nil && macval.Value != "" {
+		mac = macval.Value
+	}
+
+	if ipval != nil && ipval.Value != "" {
+		ip = ipval.Value
+	}
+
+	if ip == "" {
+		hostIP, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			return nil, err
+		}
+		ip = hostIP
 	}
 
 	h, err := hostfinder.FindByIp(ip)
 	if err != nil {
 		return nil, err
+	}
+
+	if mac != "" {
+		h.MacAddr = mac
 	}
 
 	clnt, err := clntMgr.Register(dbpool, r, h.MacAddr, h.IpAddr, h.Hostname)
