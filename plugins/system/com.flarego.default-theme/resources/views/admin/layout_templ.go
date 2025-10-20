9d91630932073ca46c16986d5d95af4b08cf0315
Adones Pitogo <pitogo.adones@gmail.com>
2025-10-21 01:48:22 +0800

    fix admin logo href

--- plugins/system/com.flarego.default-theme/resources/views/admin/layout_templ.go
+++ plugins/system/com.flarego.default-theme/resources/views/admin/layout_templ.go
@@ -71,7 +71,7 @@ func AdminLayout(api sdkapi.IPluginApi, data AdminLayoutData) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<header class=\"navbar sticky-top bg-dark flex-md-nowrap p-0 shadow\" data-bs-theme=\"dark\"><a class=\"navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white\" href=\"#\"><img class=\"logo-img\" src=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<header class=\"navbar sticky-top bg-dark flex-md-nowrap p-0 shadow\" data-bs-theme=\"dark\"><a class=\"navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white\" href=\"/admin\"><img class=\"logo-img\" src=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
