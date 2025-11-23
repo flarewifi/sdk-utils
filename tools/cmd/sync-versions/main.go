3b652f6ef1237e9116f35f45a1505544b16047b4
Adones Pitogo <pitogo.adones@gmail.com>
2025-11-24 00:52:06 +0800

    dont auto-generate release notes

--- tools/cmd/sync-versions/main.go
+++ tools/cmd/sync-versions/main.go
@@ -1,30 +1,10 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
 	"tools"
-	"tools/plugins"
-
-	sdkutils "github.com/flarehotspot/sdk-utils"
 )
 
 func main() {
 	tools.SyncCoreVersion()
 	tools.SyncGoVersion()
-	version := plugins.GetCoreInfo().Version
-	releaseNotePath := filepath.Join(sdkutils.PathCoreDir, "build", "release-notes", version+".md")
-
-	if !sdkutils.FsExists(releaseNotePath) {
-		if err := os.WriteFile(releaseNotePath, []byte("## "+formatVersion(version)+"\n\n"), 0644); err != nil {
-			panic(err)
-		}
-	}
-}
-
-func formatVersion(v string) string {
-	if v[0] != 'v' {
-		return "v" + v
-	}
-	return v
 }
