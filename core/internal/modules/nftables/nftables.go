1b0628563b86a1808f13881cd2d63f0744c8eaef
Adones Pitogo <pitogo.adones@gmail.com>
2026-02-27 02:47:58 +0800

    fix for nftables syntax

--- core/internal/modules/nftables/nftables.go
+++ core/internal/modules/nftables/nftables.go
@@ -63,11 +63,11 @@ func Setup() (err error) {
 		// Anti-tethering rules: Drop packets with TTL values indicating NAT traversal
 		// These MUST come before the accept rules to block tethered traffic
 		// Linux/Android: normal TTL=64, tethered=63,62,61,60 (up to 4 hops)
-		fmt.Sprintf("nft add rule %s %s %s ip ttl 60-63 counter log prefix \"[TETHER-BLOCK] \" drop", tableFamily, internetTable, forwardChain),
+		fmt.Sprintf("nft add rule %s %s %s ip ttl 60-63 counter log prefix '\"TETHER-BLOCK \"' drop", tableFamily, internetTable, forwardChain),
 		// Windows: normal TTL=128, tethered=127,126,125,124 (up to 4 hops)
-		fmt.Sprintf("nft add rule %s %s %s ip ttl 124-127 counter log prefix \"[TETHER-BLOCK] \" drop", tableFamily, internetTable, forwardChain),
+		fmt.Sprintf("nft add rule %s %s %s ip ttl 124-127 counter log prefix '\"TETHER-BLOCK \"' drop", tableFamily, internetTable, forwardChain),
 		// iOS/network equipment: normal TTL=255, tethered=254,253,252,251 (up to 4 hops)
-		fmt.Sprintf("nft add rule %s %s %s ip ttl 251-254 counter log prefix \"[TETHER-BLOCK] \" drop", tableFamily, internetTable, forwardChain),
+		fmt.Sprintf("nft add rule %s %s %s ip ttl 251-254 counter log prefix '\"TETHER-BLOCK \"' drop", tableFamily, internetTable, forwardChain),
 
 		// Add rules to our custom forward chain
 		// Verdict maps will accept if MAC/IP is in the map, otherwise continue to drop rule
