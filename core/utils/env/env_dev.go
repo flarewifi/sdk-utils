d918d7d0f108328d04a9cb99adc7ff05856d2d93
Adones Pitogo <adones@flarego.ph>
2026-07-06 15:07:12 +0800

    run https on port 3443

--- core/utils/env/env_dev.go
+++ core/utils/env/env_dev.go
@@ -5,7 +5,7 @@ package env
 const (
 	GO_ENV        int8   = ENV_DEV
 	HTTP_PORT     int    = 3000
-	HTTPS_PORT    int    = 443
+	HTTPS_PORT    int    = 3443
 	LocalBaseURL  string = "http://localhost:3000"
 	RPC_TOKEN     string = "xxxxxxxxxx"
 	RPC_PROXY_URL string = "http://cf-proxy.flare-local.com"
